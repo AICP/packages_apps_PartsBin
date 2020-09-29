a4282473b949d01102d7abb3261e3e0923a2100d
Ralf Luther <luther.ralf@gmail.com>
2020-09-29 17:20:23 +0200

    PartsBin: add the possibility to enable/disable the OP SoundTuner
    The devices have a build-in panoramic stero sound tunded by Dolby. Add the possibility to display a setting to turn that on or off, if it is build.
    
    Of course all credits go to the OmniROM team for all this!
    Change-Id: I02856e105f2ecd7a245a8ec21f3580cec383dfbb

--- src/com/aicp/device/DeviceSettings.java
+++ src/com/aicp/device/DeviceSettings.java
@@ -85,6 +85,7 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String KEY_DT2W_SWITCH = "double_tap_to_wake";
     public static final String KEY_S2W_SWITCH = "sweep_to_wake";
     public static final String KEY_FASTCHARGE_SWITCH = "fastcharge";
+    private static final String KEY_ENABLE_DOLBY_ATMOS = "enable_dolby_atmos";
     public static final String KEY_OFFSCREEN_GESTURES = "gesture_category";
     public static final String KEY_PANEL_SETTINGS = "panel_category";
     public static final String SLIDER_DEFAULT_VALUE = "2,1,0";
@@ -113,6 +114,7 @@ public class DeviceSettings extends PreferenceFragment implements
     private static TwoStatePreference mDoubleTapToWakeSwitch;
     private static TwoStatePreference mSweepToWakeSwitch;
 
+    private SwitchPreference mEnableDolbyAtmos;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -147,6 +149,16 @@ public class DeviceSettings extends PreferenceFragment implements
         } else {
             PreferenceCategory sliderCategory = (PreferenceCategory) findPreference(KEY_SLIDER_CATEGORY);
             sliderCategory.getParent().removePreference(sliderCategory);
+        }
+
+        boolean supports_soundtuner = getContext().getResources().
+                getBoolean(R.bool.config_device_supports_soundtuner);
+        if (supports_soundtuner) {
+            mEnableDolbyAtmos = (SwitchPreference) findPreference(KEY_ENABLE_DOLBY_ATMOS);
+            mEnableDolbyAtmos.setOnPreferenceChangeListener(this);
+        } else {
+            PreferenceCategory soundCategory = (PreferenceCategory) findPreference(KEY_ENABLE_DOLBY_ATMOS);
+            soundCategory.getParent().removePreference(soundCategory);
         }
 
         mHWKSwitch = (TwoStatePreference) findPreference(KEY_HWK_SWITCH);
@@ -317,6 +329,22 @@ public class DeviceSettings extends PreferenceFragment implements
             setSliderAction(2, sliderMode);
             int valueIndex = mSliderModeBottom.findIndexOfValue(value);
             mSliderModeBottom.setSummary(mSliderModeBottom.getEntries()[valueIndex]);
+        } else if (preference == mEnableDolbyAtmos) {
+          boolean enabled = (Boolean) newValue;
+          Intent daxService = new Intent();
+          ComponentName name = new ComponentName("com.dolby.daxservice", "com.dolby.daxservice.DaxService");
+          daxService.setComponent(name);
+          if (enabled) {
+              // enable service component and start service
+              this.getContext().getPackageManager().setComponentEnabledSetting(name,
+                      PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, 0);
+              this.getContext().startService(daxService);
+          } else {
+              // disable service component and stop service
+              this.getContext().stopService(daxService);
+              this.getContext().getPackageManager().setComponentEnabledSetting(name,
+                      PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
+          }
         }
         return true;
     }
